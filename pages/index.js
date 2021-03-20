6e57d117939eebe3bafd9060fe755a7709f6daf4
rowaxl <rowaxl0@gmail.com>
2021-03-20 11:04:07 +0900

    save works

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,18 @@ import { useState, useEffect } from 'react'
 import useSWR from 'swr'
 import { fetcher } from '../api/fetcher'
 
-const KEY = process.env['NEXT_PUBLIC_API_KEY']
-const BASE_URL = 'https://api.weatherapi.com/v1/current.json?key='
+const IS_DEV = process.env['NODE_ENV'] === 'development'
+const KEY = `?key=${process.env['NEXT_PUBLIC_API_KEY']}`
+const BASE_URL = 'https://api.weatherapi.com/v1/'
+const CURRENT_KEY = 'current.json'
+const FORECAST_KEY = 'forecast.json'
 
 export default function Home() {
   const [location, setLocation] = useState('London')
   const [buffer, setBuffer] = useState()
   const { data: locationData, error: locationError } = useSWR('/api/geo', fetcher)
-  const { data: weatherData, error: weatherError } = useSWR(BASE_URL + KEY + `&q=${location}`, fetcher)
+  const { data: currentWeather, error: currentWeatherError } = useSWR(BASE_URL + CURRENT_KEY + KEY + `&q=${location}`, fetcher)
+  const { data: forecast, error: forecastError } = useSWR(BASE_URL + FORECAST_KEY + KEY + `&q=${location}`, fetcher)
 
   useEffect(() => {
     console.log({ locationData })
@@ -17,14 +21,14 @@ export default function Home() {
   }, [locationData])
 
   useEffect(() => {
-    console.log({ weatherData })
-  }, [weatherData])
+    console.log({ weatherData: currentWeather })
+  }, [currentWeather])
 
-  if (!locationData) return <h1>Loading Geolocation data...</h1>
-  if (locationError) return <h1>Failed to get geolocation data!</h1>
+  if (!IS_DEV && !locationData) return <h1>Loading Geolocation data...</h1>
+  if (!IS_DEV && locationError) return <h1>Failed to get geolocation data!</h1>
 
-  if (!weatherData) return <h1>Fetching Weather data...</h1>
-  if (weatherError) return <h1>Failed to get weather data!</h1>
+  if (!currentWeather) return <h1>Fetching Weather data...</h1>
+  if (currentWeatherError) return <h1>Failed to get weather data!</h1>
 
   return (
     <div>
