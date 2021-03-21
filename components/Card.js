bf3f7a593f14dbb1bb21d473cc3ffb23db31b949
rowaxl <rowaxl0@gmail.com>
2021-03-21 16:38:22 +0900

    set weather data

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,7 @@
-const Card = ({ children }) => {
+const Card = ({ className, children }) => {
   return (
     <div
-      className="bg-white shadow-lg sm:rounded-3xl sm:p-20 bg-clip-padding bg-opacity-60 border border-gray-200"
+      className={"bg-white shadow-lg sm:rounded-3xl sm:p-20 bg-clip-padding bg-opacity-60 border border-gray-200" + className}
       style={{ backdropFilter: 'blur(20px)' }}
     >
       {children}
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { fetcher } from '../api/fetcher'
 import Card from '../components/Card'
 
 const IS_DEV = process.env['NODE_ENV'] === 'development'
-const KEY = `?key=${process.env['NEXT_PUBLIC_API_KEY']}`
+const WEATHER_API_KEY = `?key=${process.env['NEXT_PUBLIC_WEATHER_API_KEY']}`
 const BASE_URL = 'https://api.weatherapi.com/v1/'
 const CURRENT_KEY = 'current.json'
 const FORECAST_KEY = 'forecast.json'
@@ -12,10 +12,11 @@ const FORECAST_KEY = 'forecast.json'
 export default function Home() {
   const [location, setLocation] = useState('London')
   const [buffer, setBuffer] = useState()
+  const [tempertureUnit, setTempertureUnit] = useState('c')
 
   const { data: locationData, error: locationError } = useSWR('/api/geo', fetcher, { errorRetryCount: 3 })
-  const { data: currentWeather, error: currentWeatherError } = useSWR(BASE_URL + CURRENT_KEY + KEY + `&q=${location}`, fetcher)
-  const { data: forecast, error: forecastError } = useSWR(BASE_URL + FORECAST_KEY + KEY + `&q=${location}`, fetcher)
+  const { data: currentWeather, error: currentWeatherError } = useSWR(BASE_URL + CURRENT_KEY + WEATHER_API_KEY + `&q=${location}`, fetcher)
+  const { data: forecast, error: forecastError } = useSWR(BASE_URL + FORECAST_KEY + WEATHER_API_KEY + `&q=${location}`, fetcher)
 
   useEffect(() => {
     console.log({ locationData })
@@ -33,14 +34,67 @@ export default function Home() {
   if (currentWeatherError) return <h1>Failed to get weather data!</h1>
 
   return (
-    <div className="h-full">
-      <Card>
-        {location}
-      </Card>
+    <Card className="h-full w-full mx-12 my-6 radius-md flex flex-col">
+      <div className="flex-row">
+        <div className="flex-col">
+          <div>
+            {currentWeather.location.localtime}
+          </div>
 
-      <Card>
+          <div>
+            <img
+              src={`https:${currentWeather.current.condition.icon}`}
+            />
 
-      </Card>
-    </div>
+            <p>
+              {currentWeather.current.condition.text}
+            </p>
+          </div>
+
+          <div>
+            {currentWeather.current['temp_' + tempertureUnit]}
+          </div>
+
+          <div>
+            {currentWeather.location.country}{" "}{currentWeather.location.region}
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-row">
+        <div className="flex-col">
+          <p>
+            Weather Details
+          </p>
+
+          <div className="flex flex-row flex-wrap">
+            <div>
+              {currentWeather.current.cloud}
+            </div>
+            <div>
+              {currentWeather.current.precip_mm}
+            </div>
+            <div>
+              {currentWeather.current.humidity}
+            </div>
+            <div>
+              {currentWeather.current.uv}
+            </div>
+            <div>
+              {currentWeather.current['feelslike_' + tempertureUnit]}
+            </div>
+            <div>
+              {currentWeather.current.pressure_mb}
+            </div>
+            <div>
+              {currentWeather.current.wind_kph}
+            </div>
+            <div>
+              {currentWeather.current.vis_km}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Card>
   )
 }
